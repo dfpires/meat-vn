aa79318bb73bded0b7a967caef16deb784270171
Daniel Pires <dfpires@MacBook-Pro-de-Daniel.local>
2018-04-09 12:15:14 -0300

    v4

--- meat-v4/src/app/restaurants/restaurants.service.ts
+++ meat-v4/src/app/restaurants/restaurants.service.ts
@@ -1,20 +1,14 @@
 import {Injectable} from "@angular/core"
-
 import {Observable} from "rxjs/Observable"
-
 import {Http} from "@angular/http"
-
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
-
 import {ErrorHandler} from '../app.error-handler'
 import {Restaurant} from "./restaurant/restaurant.model"
-
 import {MEAT_API} from "../app.api"
 
 @Injectable()
 export class RestaurantsService{
-
   constructor(private http:Http){}
 
   restaurants(): Observable<Restaurant[]>{
@@ -22,4 +16,11 @@ export class RestaurantsService{
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
+
+  restaurantById(id: string): Observable<Restaurant>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
 }
